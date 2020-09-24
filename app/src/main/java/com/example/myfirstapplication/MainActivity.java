9b33e87bee0ea0d91a28e2e0999533496195b10d
sykimwind <sykimwind@gmail.com>
2020-09-24 15:16:56 +0900

    First Commit

---
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.myfirstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //여기에 기능을 만들어야 한다고요!!
+    }
+}
